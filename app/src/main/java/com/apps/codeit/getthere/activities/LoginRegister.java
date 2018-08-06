907b0f9711066ef6b83652376f348747039eeadf
Mathurin Bloworlf <blow.mathurin@gmail.com>
2018-08-06 19:24:58 -0400

    Added libraries: com.android.support:design:x.x.x

--- app/src/main/java/com/apps/codeit/getthere/activities/LoginRegister.java
+++ app/src/main/java/com/apps/codeit/getthere/activities/LoginRegister.java
@@ -1,15 +1,43 @@
 package com.apps.codeit.getthere.activities;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.apps.codeit.getthere.R;
+import com.apps.codeit.getthere.adapters.ViewPagerAdapter;
+import com.apps.codeit.getthere.fragments.Login;
+import com.apps.codeit.getthere.fragments.Register;
 
 public class LoginRegister extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_register);
+
+        toolbar = findViewById(R.id.login_register_toolbar);
+        setSupportActionBar(toolbar);
+        this.getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        ViewPager viewPager = findViewById(R.id.login_register_viewpager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = findViewById(R.id.login_register_tabs);
+        tabLayout.setSelectedTabIndicatorColor(getResources().getColor(R.color.colorAccent));
+        tabLayout.setupWithViewPager(viewPager);
+
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Login(), "Login");
+        adapter.addFragment(new Register(), "Register");
+        viewPager.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/apps/codeit/getthere/adapters/ViewPagerAdapter.java
@@ -0,0 +1,50 @@
+package com.apps.codeit.getthere.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * How to use
+ * In Activity
+ *
+ * private void setupViewPager(ViewPager viewPager) {
+ ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+ adapter.addFragment(new Your_Fragment_One(), "Fragment_One_Title");
+ adapter.addFragment(new Your_Fragment_Two(), "Fragment_Two_Title");
+ viewPager.setAdapter(adapter);
+ }
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+}
